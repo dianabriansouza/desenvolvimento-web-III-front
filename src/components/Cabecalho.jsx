72f65cda956cd7929f68ae11bd899039be3ad145
Diana Brian <diana.brian2@gmail.com>
2026-08-25 18:03:45 -0300

    Cria os primeiros componentes React

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,9 @@
+function Cabecalho() {
+ return (
+ <header>
+ <h1>Sistema de Gestão de Comércio</h1>
+ <p>Gerenciamento de produtos, clientes e funcionários</p>
+ </header>
+ )
+}
+export default Cabecalho
